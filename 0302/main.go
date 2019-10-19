0321adcefeb863d7e0b43dc80c631b5646161596
mqtsuo02 <obu.tokyo@gmail.com>
2019-10-20 04:22:25 +0900

    Added 3.2 answer

---
+++ 0302/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	var a [100]int
+
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+	}
+
+	trace(a, n)
+	insertionSort(a, n)
+}
+
+func trace(a [100]int, n int) {
+	for i := 0; i < n; i++ {
+		if i > 0 {
+			fmt.Printf(" ")
+		}
+		fmt.Printf("%d", a[i])
+	}
+	fmt.Printf("\n")
+}
+
+func insertionSort(a [100]int, n int) {
+	for i := 1; i < n; i++ {
+		v := a[i]
+		j := i - 1
+		for j >= 0 && a[j] > v {
+			a[j+1] = a[j]
+			j--
+		}
+		a[j+1] = v
+		trace(a, n)
+	}
+}
